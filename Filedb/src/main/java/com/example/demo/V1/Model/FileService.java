b7883d193e9607b5c9704bed04e6a22756e283dd
VigneshChintakindi <vigneshchintakindi1234@gmail.com>
2023-01-30 12:44:31 +0530

    Service Impl Changes

--- Filedb/src/main/java/com/example/demo/V1/Model/FileService.java
+++ Filedb/src/main/java/com/example/demo/V1/Model/FileService.java
@@ -1,5 +1,13 @@
 package com.example.demo.V1.Model;
 
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FileService {
 
+	Filedb store(MultipartFile file) throws IOException ;
+	Filedb getFile(String id);
+	Stream<Filedb> getAllFiles();
 }
--- Filedb/src/main/java/com/example/demo/V1/Model/FileServiceImpl.java
+++ Filedb/src/main/java/com/example/demo/V1/Model/FileServiceImpl.java
@@ -9,22 +9,24 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class FileServiceImpl {
+public class FileServiceImpl implements FileService{
 	
 	@Autowired
 	  private FileRepo fileRepo;
 
+	@Override
 	  public Filedb store(MultipartFile file) throws IOException {
 	    String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 	    Filedb Filedb = new Filedb(fileName, file.getContentType(), file.getBytes());
 
 	    return fileRepo.save(Filedb);
 	  }
-
+	@Override
 	  public Filedb getFile(String id) {
 	    return fileRepo.findById(id).get();
 	  }
 	  
+	@Override
 	  public Stream<Filedb> getAllFiles() {
 	    return fileRepo.findAll().stream();
 	  }
